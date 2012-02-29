22d106f9c71b8ae7743f42ba39f46abf2ea2e24d
ralf <ralf@henschkowski.com>
2012-02-29 20:05:33 +0100

    fix refcouting in unsol handler

--- tuxmodule.c
+++ tuxmodule.c
@@ -2224,6 +2224,9 @@ static PyObject* tux_tpbroadcast(PyObject* self, PyObject* arg) {
 static void unsol_handler(char* tuxbuf, long len, long flags) {
 
     PyObject* data_py = NULL;
+    /* Obtain the Global Interpreter Lock */
+    PyGILState_STATE gstate;
+    gstate = PyGILState_Ensure();
 
     /* Transform the TUXEDO buffer to a Python type (len is not needed
        (only STRING/FML32 is supported), flags is not supported by TUXEDO */
@@ -2233,14 +2236,12 @@ static void unsol_handler(char* tuxbuf, long len, long flags) {
 	goto leave_func;
     }
 
-    /* Obtain the Global Interpreter Lock */
-    PyGILState_STATE gstate;
-    gstate = PyGILState_Ensure();
     PyObject_CallFunction (py_unsol_handler, "O", data_py); 
     /* Release the thread. No Python API allowed beyond this point. */
     PyGILState_Release(gstate);    
 
  leave_func:
+    Py_DECREF(data_py);
     
     return;
 }
